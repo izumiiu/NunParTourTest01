4f7d8821777488a37ab3be0816ddd3aedb165e64
izumiiu <peerawat.ez@gmail.com>
2026-05-29 00:54:37 +0700

    add notfinished register page

---
+++ app/register/page.tsx
@@ -0,0 +1,13 @@
+import Link from "next/link";
+
+const Register = () => {
+    return(
+        <div>
+            <h1>Did not build Register page yet🤡</h1>
+            <br />
+            <h3><Link href={"/"}>Goback to Home Page</Link></h3>
+        </div>
+    );
+}
+
+export default Register;
